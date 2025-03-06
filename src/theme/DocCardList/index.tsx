74c5688cd65a38c8012a5923c5fa0a62da7570ed
Omer Ozceylan <42185275+omerozceylan@users.noreply.github.com>
2025-03-06 22:01:13 +0300

    changes

---
+++ src/theme/DocCardList/index.tsx
@@ -0,0 +1,14 @@
+import React, {type ReactNode} from 'react';
+import DocCardList from '@theme-original/DocCardList';
+import type DocCardListType from '@theme/DocCardList';
+import type {WrapperProps} from '@docusaurus/types';
+
+type Props = WrapperProps<typeof DocCardListType>;
+
+export default function DocCardListWrapper(props: Props): ReactNode {
+  return (
+    <div className="text-white">
+lorem ipsum      <DocCardList {...props} />
+    </div>
+  );
+}
